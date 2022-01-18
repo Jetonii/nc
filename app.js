d699d52edcf24aead14d532c83a348960dad85fd
Jetonii <70644726+Jetonii@users.noreply.github.com>
2022-01-18 10:23:21 +0100

    Updated app to work on phones

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ const binary = document.getElementById('binary')
 const result = document.getElementById('result')
 const decimal = document.getElementById('decimal')
 const octal = document.getElementById('octal')
+const binarySubmit = document.getElementById('binarySubmit')
+const octalSubmit = document.getElementById('octalSubmit')
+const decimalSubmit = document.getElementById('decimalSubmit')
+const hexaSubmit = document.getElementById('hexaSubmit')
 
-binary.addEventListener('keyup', function (event) {
-    if (event.key === 'Enter' && event.keycode === 13) {
+
+
+binary.addEventListener("keyup", function (event) {
+    if (event.key === 'Enter') {
         event.preventDefault();
         hexa.value = parseInt(binary.value, 2).toString(16).toUpperCase()
         decimal.value = parseInt(binary.value, 2)
@@ -13,8 +19,9 @@ binary.addEventListener('keyup', function (event) {
     }
 });
 
-hexa.addEventListener('keyup', function (event) {
-    if (event.key === 'Enter' && event.keycode === 13) {
+
+hexa.addEventListener("keyup", function (event) {
+    if (event.key === 'Enter') {
         event.preventDefault();
         decimal.value = parseInt(hexa.value, 16)
         binary.value = (parseInt(hexa.value, 16).toString(2)).padStart(9, '0');
@@ -22,8 +29,8 @@ hexa.addEventListener('keyup', function (event) {
     }
 });
 
-decimal.addEventListener('keyup', function (event) {
-    if (event.key === 'Enter' && event.keycode === 13) {
+decimal.addEventListener("keyup", function (event) {
+    if (event.key === 'Enter') {
         event.preventDefault();
         hexa.value = parseInt(decimal.value).toString(16).toUpperCase();
         binary.value = parseInt(decimal.value).toString(2)
@@ -31,11 +38,37 @@ decimal.addEventListener('keyup', function (event) {
     }
 });
 
-octal.addEventListener('keyup', function (event) {
-    if (event.key === 'Enter' && event.keycode === 13) {
+
+octal.addEventListener("keyup", function (event) {
+    if (event.key === 'Enter') {
         event.preventDefault();
         hexa.value = parseInt(parseInt(octal.value, '8')).toString(16).toUpperCase();
         decimal.value = parseInt(octal.value, '8')
         binary.value = parseInt(parseInt(octal.value, '8')).toString(2)
     }
-});
+})
+
+binarySubmit.addEventListener('click', (event) => {
+    event.preventDefault();
+    hexa.value = parseInt(binary.value, 2).toString(16).toUpperCase()
+    decimal.value = parseInt(binary.value, 2)
+    octal.value = parseInt(binary.value, 2).toString(8)
+})
+hexaSubmit.addEventListener('click', (event) => {
+    event.preventDefault();
+    decimal.value = parseInt(hexa.value, 16)
+    binary.value = (parseInt(hexa.value, 16).toString(2)).padStart(9, '0');
+    octal.value = parseInt(hexa.value, 16).toString(8)
+})
+octalSubmit.addEventListener('click', (event) => {
+    event.preventDefault();
+    hexa.value = parseInt(parseInt(octal.value, '8')).toString(16).toUpperCase();
+    decimal.value = parseInt(octal.value, '8')
+    binary.value = parseInt(parseInt(octal.value, '8')).toString(2)
+})
+decimalSubmit.addEventListener('click', (event) => {
+    event.preventDefault();
+    hexa.value = parseInt(decimal.value).toString(16).toUpperCase();
+    binary.value = parseInt(decimal.value).toString(2)
+    octal.value = parseInt(decimal.value).toString(8);
+})
